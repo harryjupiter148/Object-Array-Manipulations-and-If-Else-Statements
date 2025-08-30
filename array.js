b35c4a2f19e982dcf21f334b447bec3a643ceb4b
Harry Jupiter <72943529+harryjupiter148@users.noreply.github.com>
2025-08-30 23:46:12 +0100

    Update and rename assgnt.js to array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+//Array manipulation
+Array = [10, 50, 90, 1, 70, 3.14, 80, 6.5, 40, 20];
+console.log(Array);
+
+Array.pop();
+Array.pop();
+console.log(Array);
+Array[0] = "Raven";
+Array[1] = "Panther";
+Array[2] = "Antelope";
+Array[3] = "Mantis Shrimp";
+Array[4] = "Hyena";
+Array[5] = "Ostrich";
+Array[6] = "Wren";
+Array[7] = "Egret";
+console.log(Array);
